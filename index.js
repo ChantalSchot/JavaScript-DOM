5038d48ff4499128b2a5e4d59ccd3398be894c63
Chantal <chantalschot@outlook.com>
2019-09-22 12:35:14 +0200

    Section 10 lesson 122-125

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ document.querySelectorAll(".list")[2].innerHTML = "Chantal";
 
 
 document.querySelector("li a").style.color = "red";
+
+document.querySelector(".btn").style.backgroundColor = "yellow";
+
+document.querySelector("h1").classList.add("huge");
